b1b891b5fd0e69c54e40d8fff436ccec003f7554
zander mackenzie <amacke201@caledonian.ac.uk>
2018-05-12 21:37:23 +0100

    initial upload

---
+++ js/js.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+    $('.dv-carousel').slick({
+      dots: false,
+      infinite: true,
+      speed: 800,
+      slidesToShow: 1,
+      adaptiveHeight: true,
+      autoplay: true,
+      arrows: false
+    });
+});
